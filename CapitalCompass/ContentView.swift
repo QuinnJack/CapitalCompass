19e8506b849d94c20096db2e35ba53ab1ffa20b9
QuinnJack <108008843+QuinnJack@users.noreply.github.com>
2024-01-06 14:29:46 -0500

    Add carousel view

--- CapitalCompass/ContentView.swift
+++ CapitalCompass/ContentView.swift
@@ -2,33 +2,30 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
-    @State private var cameraPosition: MKCoordinateRegion = .userRegion
+    @State private var cameraPosition: MapCameraPosition = .region(.userRegion)
     let buttons = [("house", "Home"), ("person", "Profile"), ("gear", "Settings"), ("questionmark.circle", "Help")]
     @State private var selectedButton = "Home"
     @State private var searchText = ""
     @State private var results = [MKMapItem]()
-
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $cameraPosition)
-                .overlay(alignment: .top) {
-                    SearchView(searchText: $searchText, icon: .constant("magnifyingglass"), placeHolder: .constant("Search Ottawa"))
+            Map(position: $cameraPosition)
+                .mapControls {
+                    MapCompass()
+                    MapUserLocationButton()
                 }
-                .onSubmit(of: .text) {
-                    Task {
-                        await searchPlaces()
-                    }
-                }
-
-            ForEach(results, id: \.self) { item in
-                let placemark = item.placemark
-                Marker(placemark.name ??  "", coordinate: placemark.coordinate)
+            .overlay(alignment: .top) {
+                SearchView(searchText: $searchText, icon: .constant("magnifyingglass"), placeHolder: .constant("Search Ottawa"))
             }
-
+            .onSubmit(of: .text){
+                print("search \(searchText)")
+                Task{ await searchPlaces()}
+            }
             VStack {
                 Spacer()
 
                 HStack(spacing: 0) {
+                    
                     ForEach(buttons, id: \.1) { (icon, title) in
                         Button(action: {
                             self.selectedButton = title
@@ -47,10 +44,10 @@ struct ContentView: View {
                                     .bold()
                             }
                             .foregroundStyle(.white)
-                            .background(
-                                RadialGradient(gradient: Gradient(colors: [.blue, .purple]), center: .center, startRadius: 20, endRadius: 100)
-                            )
-                            .clipShape(RoundedRectangle(cornerRadius: 14))
+                                .background(
+                                    RadialGradient.gradientDoneSteps
+                                )
+                            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 14, height: 14)))
                             .shadow(color: .primary.opacity(0.7), radius: 2, x: 0, y: 2)
                             .scaleEffect(0.8)
                             .offset(y: 14)
@@ -62,30 +59,30 @@ struct ContentView: View {
             }
         }
     }
+}
 
+extension ContentView {
     func searchPlaces() async {
         let request = MKLocalSearch.Request()
         request.naturalLanguageQuery = searchText
         request.region = .userRegion
-        let response = try? await MKLocalSearch(request: request).start()
-        self.results = response?.mapItems ?? []
+        let results = try? await MKLocalSearch(request: request).start()
+        self.results = results?.mapItems ?? []
     }
 }
 
+
 extension CLLocationCoordinate2D {
     static var userLocation: CLLocationCoordinate2D {
-        .init(latitude: 45.415, longitude: -75.6972)
-    }
+        return .init(latitude: 45.415, longitude: -75.6972)
+}
 }
 
 extension MKCoordinateRegion {
     static var userRegion: MKCoordinateRegion {
-        .init(center: .userLocation, latitudinalMeters: 5000, longitudinalMeters: 5000)
+        return .init(center: .userLocation, latitudinalMeters: 5000, longitudinalMeters: 5000)
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+#Preview {
+    ContentView()
 }
